ea39b3ba571b45387848082453bb3f67c61f2ca4
cjameszhang <cjameszhang@gmail.com>
2014-10-18 15:48:00 -0500

    add html files

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -11,6 +11,13 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^$', hello.views.index, name='index'),
+    url(r'^login/?', hello.views.login, name='login'),
+    url(r'^register/?', hello.views.register, name='register'),
+    url(r'^add/?', hello.views.add, name='add'),
+    url(r'^account/?', hello.views.account, name='account'),
+    url(r'^[0-9]+/?', hello.views.profile, name='profile'),
+    url(r'^logout/?', hello.views.logout, name='logout'),
+
     url(r'^db', hello.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
 
--- hello/views.py
+++ hello/views.py
@@ -5,9 +5,27 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', {'user': None})
+    return render(request, 'index.html', {'user': None, 'request': request})
 
+def login(request):
+    return render(request, 'login.html', {'user': None, 'request': request})
+    
+def logout(request):
+    return render(request, 'logout.html', {'user': None, 'request': request})
+    
+def register(request):
+    return render(request, 'register.html', {'user': None, 'request': request})
+    
+def account(request):
+    return render(request, 'account.html', {'user': None, 'request': request})
+    
+def add(request):
+    return render(request, 'add.html', {'user': None, 'request': request})
 
+def profile(request):
+    return render(request, 'profile.html', {'user': None, 'request': request})
+
+# TODO: delete
 def db(request):
 
     greeting = Greeting()
