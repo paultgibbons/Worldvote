c2ea9da274f5f9c1c6b78baf86b5a92d6d2b95ee
Paul Gibbons <paultgibbons@gmail.com>
2014-10-19 16:16:01 -0500

    add image upload

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -99,6 +99,9 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = '/Users/paultgibbons/media/'
+MEDIA_URL = '/media/'
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
