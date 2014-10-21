eae250981a765e6a36e54bb1e81452f0d8dbfd87
cjameszhang <cjameszhang@gmail.com>
2014-10-20 21:27:13 -0500

    change db

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -61,7 +61,8 @@ WSGI_APPLICATION = 'gettingstarted.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'db.sqlite3',
     }
 }
 
